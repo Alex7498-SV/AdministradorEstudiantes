40eb40e7a5a9b09febb99bb614819faf9b3f9278
NitroSkate <00159817@uca.edu.sv>
2020-06-29 22:38:49 -0600

    .

--- src/main/java/com/student/administrador/config/JPAConfiguration.java
+++ src/main/java/com/student/administrador/config/JPAConfiguration.java
@@ -47,7 +47,7 @@ public class JPAConfiguration {
 		dataSource.setDriverClassName("org.postgresql.Driver");
 		dataSource.setUrl("jdbc:postgresql://127.0.0.1:5432/DBEstudianteAdmin");
 		dataSource.setUsername("postgres");
-		dataSource.setPassword("canela01");
+		dataSource.setPassword("12345678");
 		return dataSource;
 	}
 	
