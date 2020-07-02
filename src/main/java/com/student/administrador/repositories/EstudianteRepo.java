dae17d887a4fb2b0a5790e6791b60e7ab5e8f603
MaPache07 <00170517@uca.edu.sv>
2020-07-02 17:44:30 -0600

    Se ha cambiado la query de EstudianteRepo, quedo fea

--- src/main/java/com/student/administrador/repositories/EstudianteRepo.java
+++ src/main/java/com/student/administrador/repositories/EstudianteRepo.java
@@ -11,15 +11,20 @@ import com.student.administrador.domain.Estudiante;
 public interface EstudianteRepo extends JpaRepository<Estudiante, Integer> {
 	
 	@Query(nativeQuery=true,
-		value="SELECT e.nombres, e.apellidos,  A.Aprobadas, A.Reprobadas, A.Promedio"
-		+	"FROM public.estudiante e, public.estudiantemateria me, public.materia mat,"
-		+					"(SELECT COUNT(CASE WHEN mee.nota>=6 THEN 1 WHEN mee.nota=null THEN 0 END) as Aprobadas, COUNT(CASE WHEN mee.nota < 6 THEN 1 WHEN mee.nota=null THEN 0 END) as Reprobadas, CASE WHEN mee.nota!=null THEN SUM(mee.nota)/COUNT(mee.idEstudiante) END as Promedio, mee.idEstudiante as IDd" 
-		+					"FROM public.estudiantemateria mee, public.estudiante ee"
-		+					"WHERE mee.idEstudiante=ee.idEstudiante and (ee.nombres LIKE ?1% OR ee.apellidos LIKE ?2%) group by mee.idEstudiante) A"
-		+	"WHERE e.idEstudiante = me.idEstudiante and"
-		+		"me.idMateria = mat.idMateria and"
-		+		"me.idEstudiante = A.IDd and"
-		+		"(e.nombres LIKE ?1% OR e.apellidos LIKE ?2%)"
-		+	"GROUP BY e.nombres, e.apellidos, A.Aprobadas, A.reprobadas, A.Promedio")
+		value="SELECT e.nombres, e.apellidos,  A.Aprobadas, A.Reprobadas, A.Promedio\r\n" + 
+				"FROM public.estudiante e\r\n" + 
+				"	LEFT JOIN public.estudiantemateria em ON e.idEstudiante=em.idEstudiante\r\n" + 
+				"	LEFT JOIN public.materia mat ON em.idMateria=mat.idMateria\r\n" + 
+				"	LEFT JOIN \r\n" + 
+				"	(SELECT sum(CASE WHEN mee.nota>=6 THEN 1 END) as Aprobadas, COUNT(CASE WHEN mee.nota < 6 THEN 1 END) as Reprobadas, SUM(mee.nota)/COUNT(mee.idEstudiante) as Promedio, mee.idEstudiante as IDd \r\n" + 
+				"		FROM public.estudiante ee\r\n" + 
+				"			LEFT JOIN public.estudiantemateria mee ON ee.idEstudiante=mee.idEstudiante\r\n" + 
+				"	 		LEFT JOIN public.materia matt ON mee.idMateria=matt.idMateria \r\n" + 
+				"	 	WHERE\r\n" + 
+				"		 	(e.nombres LIKE ?1% OR e.apellidos LIKE '?2%')\r\n" + 
+				"	 	GROUP BY mee.idEstudiante) A ON em.idEstudiante = A.IDd \r\n" + 
+				"WHERE\r\n" + 
+				"	(e.nombres LIKE ?1% OR e.apellidos LIKE '?2%')\r\n" + 
+				"GROUP BY e.nombres, e.apellidos, A.Aprobadas, A.reprobadas, A.Promedio")
 	public List<Object[]> expedientePorNombreApellido(String nombre, String apellido) throws DataAccessException;
 }
